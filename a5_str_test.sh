d04ecc441c7d9fab23c54277479b2146c7f3b0c5
“wangruiguo” <wangrg@dchealth.com>
2022-12-16 17:43:33 +0800

    learn shell

---
+++ a5_str_test.sh
@@ -0,0 +1,17 @@
+str_test='This is a str''aha aha ..'
+echo $str_test
+
+your_name='Deep_White'
+str="I know you ,your name is $your_name \n"
+echo $str
+
+hello="hello, $your_name \n"
+echo $hello
+hello_1="hello1, ${your_name} \n"
+echo $hello_1
+
+greeting_2='hello, '$your_name' \n'
+echo $greeting_2
+
+greeting_3='hello, ${your_name} \n'
+echo $greeting_3
